5db06c69267c5fafe7393e4d4d9ea442b1b3703b
Zhe Zheng <shanonlinecn@yahoo.com>
2015-08-22 11:38:18 -0500

    0822.2

--- LEDControl/src/com/example/ledcontrol/LoginActivity.java
+++ LEDControl/src/com/example/ledcontrol/LoginActivity.java
@@ -1,9 +1,44 @@
 package com.example.ledcontrol;
 
+
+import static org.solemnsilence.util.Py.list;
+import static org.solemnsilence.util.Py.truthy;
+//import io.spark.core.android.R;
+import com.example.ledcontrol.R;
+import io.spark.core.android.cloud.ApiFacade;
+import io.spark.core.android.cloud.requestservice.SimpleSparkApiService;
+//import io.spark.core.android.ui.corelist.CoreListActivity;
+import io.spark.core.android.ui.util.Ui;
+import io.spark.core.android.util.NetConnectionHelper;
+
+
+import org.apache.http.HttpStatus;
+import org.solemnsilence.util.EZ;
+import org.solemnsilence.util.TLog;
+
+
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.text.TextUtils;
+import android.util.Patterns;
+import android.view.Gravity;
+import android.view.KeyEvent;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.View.OnTouchListener;
+import android.view.inputmethod.EditorInfo;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ScrollView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class LoginActivity extends Activity {
 
